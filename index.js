2e4a9ee658a1ccec4c8e3b3a6d7d3d014c34968f
shivamkumar011 <79047931+shivamkumar011@users.noreply.github.com>
2021-11-01 15:46:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById('demo').innerHTML=`You can use script tag inside an HTML web page to add JavaScript code in the following ways
+
+ <br/> <p> In the HEAD element (<head>...</head>).The script placed inside the head element is loaded with the webpage and gets executed if any defined event occurs.</p>
+
+ <br/><p> In the BODY element (<body>...</body>).You can place a script tag inside the body element of an HTML document too. The script tag inside the body element runs when a web page starts loading in a web browser. </p>
+
+<br/> <p>To include an External JavaScript File.
+Create external JavaScript file with the extension .js. After creating, add it to the HTML file in the script tag. The src attribute is used to include that external JavaScript file.
+
+<p/>
+`;
+
